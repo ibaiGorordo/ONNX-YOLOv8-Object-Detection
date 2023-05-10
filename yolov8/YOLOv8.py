0da4e318fad9191118f3e989271285dd7b57b871
Semih Demirel <85176438+semihhdemirel@users.noreply.github.com>
2023-05-10 11:34:06 +0300

    fix typo

--- yolov8/YOLOv8.py
+++ yolov8/YOLOv8.py
@@ -125,17 +125,17 @@ if __name__ == '__main__':
 
     model_path = "../models/yolov8m.onnx"
 
-    # Initialize YOLOv7 object detector
-    yolov7_detector = YOLOv8(model_path, conf_thres=0.3, iou_thres=0.5)
+    # Initialize YOLOv8 object detector
+    yolov8_detector = YOLOv8(model_path, conf_thres=0.3, iou_thres=0.5)
 
     img_url = "https://live.staticflickr.com/13/19041780_d6fd803de0_3k.jpg"
     img = imread_from_url(img_url)
 
     # Detect Objects
-    yolov7_detector(img)
+    yolov8_detector(img)
 
     # Draw detections
-    combined_img = yolov7_detector.draw_detections(img)
+    combined_img = yolov8_detector.draw_detections(img)
     cv2.namedWindow("Output", cv2.WINDOW_NORMAL)
     cv2.imshow("Output", combined_img)
     cv2.waitKey(0)
