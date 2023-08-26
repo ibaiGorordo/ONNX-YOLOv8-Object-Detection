164bd46232ef0fd0dca95379a9bd66ad289a45f8
Ibai Gorordo <ibai.gorordo@hotmail.com>
2023-08-26 11:26:06 +0900

    Update utils.py

--- yolov8/utils.py
+++ yolov8/utils.py
@@ -70,73 +70,55 @@ def xywh2xyxy(x):
 
 
 def draw_detections(image, boxes, scores, class_ids, mask_alpha=0.3):
-    mask_img = image.copy()
     det_img = image.copy()
 
     img_height, img_width = image.shape[:2]
-    size = min([img_height, img_width]) * 0.0006
+    font_size = min([img_height, img_width]) * 0.0006
     text_thickness = int(min([img_height, img_width]) * 0.001)
 
+    det_img = draw_masks(det_img, boxes, class_ids, mask_alpha)
+
     # Draw bounding boxes and labels of detections
-    for box, score, class_id in zip(boxes, scores, class_ids):
+    for class_id, box, score in zip(class_ids, boxes, scores):
+        color = colors[class_id]
+
+        draw_box(det_img, box, color)
+
+        label = class_names[class_id]
+        caption = f'{label} {int(score * 100)}%'
+        draw_text(det_img, caption, box, color, font_size, text_thickness)
+
+    return det_img
+
+
+def draw_box( image: np.ndarray, box: np.ndarray, color: tuple[int, int, int] = (0, 0, 255),
+             thickness: int = 2) -> np.ndarray:
+    x1, y1, x2, y2 = box.astype(int)
+    return cv2.rectangle(image, (x1, y1), (x2, y2), color, thickness)
+
+
+def draw_text(image: np.ndarray, text: str, box: np.ndarray, color: tuple[int, int, int] = (0, 0, 255),
+              font_size: float = 0.001, text_thickness: int = 2) -> np.ndarray:
+    x1, y1, x2, y2 = box.astype(int)
+    (tw, th), _ = cv2.getTextSize(text=text, fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                                  fontScale=font_size, thickness=text_thickness)
+    th = int(th * 1.2)
+
+    cv2.rectangle(image, (x1, y1),
+                  (x1 + tw, y1 - th), color, -1)
+
+    return cv2.putText(image, text, (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, font_size, (255, 255, 255), text_thickness, cv2.LINE_AA)
+
+def draw_masks(image: np.ndarray, boxes: np.ndarray, classes: np.ndarray, mask_alpha: float = 0.3) -> np.ndarray:
+    mask_img = image.copy()
+
+    # Draw bounding boxes and labels of detections
+    for box, class_id in zip(boxes, classes):
         color = colors[class_id]
 
         x1, y1, x2, y2 = box.astype(int)
 
-        # Draw rectangle
-        cv2.rectangle(det_img, (x1, y1), (x2, y2), color, 2)
-
         # Draw fill rectangle in mask image
         cv2.rectangle(mask_img, (x1, y1), (x2, y2), color, -1)
 
-        label = class_names[class_id]
-        caption = f'{label} {int(score * 100)}%'
-        (tw, th), _ = cv2.getTextSize(text=caption, fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                                      fontScale=size, thickness=text_thickness)
-        th = int(th * 1.2)
-
-        cv2.rectangle(det_img, (x1, y1),
-                      (x1 + tw, y1 - th), color, -1)
-        cv2.rectangle(mask_img, (x1, y1),
-                      (x1 + tw, y1 - th), color, -1)
-        cv2.putText(det_img, caption, (x1, y1),
-                    cv2.FONT_HERSHEY_SIMPLEX, size, (255, 255, 255), text_thickness, cv2.LINE_AA)
-
-        cv2.putText(mask_img, caption, (x1, y1),
-                    cv2.FONT_HERSHEY_SIMPLEX, size, (255, 255, 255), text_thickness, cv2.LINE_AA)
-
-    return cv2.addWeighted(mask_img, mask_alpha, det_img, 1 - mask_alpha, 0)
-
-
-def draw_comparison(img1, img2, name1, name2, fontsize=2.6, text_thickness=3):
-    (tw, th), _ = cv2.getTextSize(text=name1, fontFace=cv2.FONT_HERSHEY_DUPLEX,
-                                  fontScale=fontsize, thickness=text_thickness)
-    x1 = img1.shape[1] // 3
-    y1 = th
-    offset = th // 5
-    cv2.rectangle(img1, (x1 - offset * 2, y1 + offset),
-                  (x1 + tw + offset * 2, y1 - th - offset), (0, 115, 255), -1)
-    cv2.putText(img1, name1,
-                (x1, y1),
-                cv2.FONT_HERSHEY_DUPLEX, fontsize,
-                (255, 255, 255), text_thickness)
-
-
-    (tw, th), _ = cv2.getTextSize(text=name2, fontFace=cv2.FONT_HERSHEY_DUPLEX,
-                                  fontScale=fontsize, thickness=text_thickness)
-    x1 = img2.shape[1] // 3
-    y1 = th
-    offset = th // 5
-    cv2.rectangle(img2, (x1 - offset * 2, y1 + offset),
-                  (x1 + tw + offset * 2, y1 - th - offset), (94, 23, 235), -1)
-
-    cv2.putText(img2, name2,
-                (x1, y1),
-                cv2.FONT_HERSHEY_DUPLEX, fontsize,
-                (255, 255, 255), text_thickness)
-
-    combined_img = cv2.hconcat([img1, img2])
-    if combined_img.shape[1] > 3840:
-        combined_img = cv2.resize(combined_img, (3840, 2160))
-
-    return combined_img
+    return cv2.addWeighted(mask_img, mask_alpha, image, 1 - mask_alpha, 0)
